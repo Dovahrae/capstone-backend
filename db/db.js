3ec5a6e183cd2095a251afc5790d110128b91c35
Dovahrae <167934960+Dovahrae@users.noreply.github.com>
2024-09-18 18:34:31 -0400

    adding some sequelize i guess

--- db/db.js
+++ db/db.js
@@ -1,16 +1,16 @@
 import Sequelize from "sequelize";
 
-const db = new Sequelize("postgres://localhost:5432/capstone");
+let db;
 
 // process.env.DATABASE_URL
 // if it's not working try taking out the consts and add "let db;"
-if (process.env.DATABASE_URL === undefinded) {
+if (process.env.DATABASE_URL === undefined) {
     console.log("connected locally");
-    const db = new Sequelize("postgres://localhost:5432/capstone", {
+    db = new Sequelize("postgres://localhost:5432/capstone", {
         logging: false,
     });
 } else {
-    const db = new Sequelize(process.env.DATABASE_URL, {
+    db = new Sequelize(process.env.DATABASE_URL, {
         logging: false,
     });
 }
